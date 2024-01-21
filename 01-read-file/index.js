fedd0507b6fb2b6f93d5338f4d97a3e5db8954f7
Ivan <ivankonovalov96@inbox.ru>
2024-01-21 20:03:30 +0500

    feat: add solution 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,7 @@
-console.log('Hello');
+const { stdout } = process;
+const path = require('path');
+const absPath = path.resolve(__dirname, 'text.txt');
+const fs = require('fs');
+const readableStream = fs.createReadStream(absPath, 'utf-8');
+
+readableStream.on('data', (chunk) => stdout.write(chunk));
